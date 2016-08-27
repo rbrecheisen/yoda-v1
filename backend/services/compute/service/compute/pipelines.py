96f6156d28965c0b9e923f15b3a0977fad832818
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2016-08-27 15:00:01 +0200

    Celery tasks now functions of pipeline objects

--- backend/services/compute/service/compute/pipelines.py
+++ backend/services/compute/service/compute/pipelines.py
@@ -1,33 +1,17 @@
 import time
 from celery import chord, shared_task
-from celery.result import AsyncResult
-
-
-@shared_task
-def get_task_result_ids(ids):
-    return ids
-
-
-@shared_task
-def train_classifier(file_id, params):
-    print('Training classifier on file {} with parameters {}'.format(file_id, params))
-    time.sleep(5)
-    return file_id + 100
 
 
 # ----------------------------------------------------------------------------------------------------------------------
 class Pipeline(object):
 
-    @staticmethod
-    def task_status(task_id):
-        return AsyncResult(task_id).status
-
-    @staticmethod
-    def task_result(task_id):
-        return AsyncResult(task_id).result
-
     def run(self, params):
         raise NotImplementedError()
+
+    @staticmethod
+    @shared_task
+    def get_task_result_ids(ids):
+        return ids
 
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -37,27 +21,33 @@ class ClassifierTrainingPipeline(Pipeline):
     information about its accuracy.
     """
     def run(self, params):
-
         print('Running classifier training pipeline')
+        self.validate_params(params)
+        # Create training task for each file ID (here only 1)
+        tasks = []
+        for file_id in params['file_ids']:
+            tasks.append(self.train_classifier.subtask((file_id, params)))
+        # Create chord task that collects the file IDs
+        job = chord(header=tasks, body=self.get_task_result_ids.subtask())
+        result = job.apply_async()
+        # Return callback task ID
+        return result.task_id
 
-        # Check mandatory parameters are valid and complete
+    @staticmethod
+    @shared_task
+    def train_classifier(file_id, params):
+        print('Training classifier on file {} with parameters {}'.format(file_id, params))
+        time.sleep(5)
+        return file_id + 100
+
+    @staticmethod
+    def validate_params(params):
         assert 'file_ids' in params.keys()
         assert len(params['file_ids']) == 1
         assert params['file_ids'][0] > 0
         assert 'classifier' in params.keys()
         assert 'name' in params['classifier'].keys()
         assert params['classifier']['name'] in ['svm-lin', 'svm-rbf']
-
-        # Run classifier training
-        tasks = []
-        for file_id in params['file_ids']:
-            tasks.append(train_classifier.subtask((file_id, params)))
-        job = chord(header=tasks, body=get_task_result_ids.subtask())
-        result = job.apply_async()
-
-        # Return callback task ID
-        print('Pipeline finished')
-        return result.task_id
 
 
 # ----------------------------------------------------------------------------------------------------------------------
