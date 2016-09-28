77facfcd7097f2477f529601aaedc28c50f2984f
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2016-09-28 22:52:29 +0200

    Saving work

---
+++ backend/services/rabbitmq/run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+eval $(docker-machine env default)
+
+# Run rabbitmq server in Docker container...
---
+++ backend/services/redis/run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+eval $(docker-machine env default)
+
+# Run redis server in Docker container....
