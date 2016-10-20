9916aa136fb26a4870aee85afad1a8571601a6fd
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2016-10-20 09:27:36 +0200

    Saving work

--- backend/services/ui/static/js/environ.js
+++ backend/services/ui/static/js/environ.js
@@ -43,3 +43,6 @@ window.environ.UI_SERVICE_PORT = '80';
 window.environ = {};
 window.environ.UI_SERVICE_HOST = '192.168.99.100';
 window.environ.UI_SERVICE_PORT = '80';
+window.environ = {};
+window.environ.UI_SERVICE_HOST = '192.168.99.100';
+window.environ.UI_SERVICE_PORT = '80';
